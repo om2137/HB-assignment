e4eddfcf68bfc44d68d34834cf5bd5d19b39fa30
om2137 <omraut961@gmail.com>
2024-01-01 12:29:39 +0530

    base app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Weather from './components/weather'
-import LocationToLatLng from './components/geocoding';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-box">
         <Weather />
-      </header>
+      </div>
     </div>
   );
 }
